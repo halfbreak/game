4dd6916445d4e09319d20d53355f90958304bb50
halfbreak <14543067+halfbreak@users.noreply.github.com>
2018-10-04 20:31:03 +0200

    Updating the README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# game
+# Game
+
+## Running the code
+
+`./gradlew build && java -jar build/libs/game-0.1.0-SNAPSHOT.jar`
+
+## The game
+
+It is a very simple command line based game. You are given options and must choose between them.
+There are no major decisions and the story line is made to be linear.
+
+You can save the game and load it back by passing the path to the save file.
--- src/main/java/com/hlbk/game/character/Character.java
+++ src/main/java/com/hlbk/game/character/Character.java
@@ -3,10 +3,6 @@ package com.hlbk.game.character;
 import com.hlbk.game.world.items.Fists;
 import com.hlbk.game.world.items.Weapon;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.StringJoiner;
-
 public class Character {
 
     private final String characterName;
@@ -57,27 +53,6 @@ public class Character {
 
     Integer getCurrentChapter() {
         return currentChapter;
-    }
-
-    //TODO move these two methods to a Codec class
-    public String serialize() {
-        StringJoiner stringJoiner = new StringJoiner(",");
-        stringJoiner.add(characterName);
-        stringJoiner.add(Long.toString(experience.getExperience()));
-        stringJoiner.add(weapon.getClass().getCanonicalName());
-        stringJoiner.add(currentChapter.toString());
-        return stringJoiner.toString();
-    }
-
-    public static Character deserialize(String stringFromFile) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        String[] fields = stringFromFile.split(",");
-        String characterName = fields[0];
-        long experience = Long.parseLong(fields[1]);
-        Class<?> clazz = Class.forName(fields[2]);
-        Constructor<?> constructor = clazz.getConstructor();
-        Weapon instance = (Weapon) constructor.newInstance();
-        int currentChapter = Integer.parseInt(fields[3]);
-        return new Character(characterName, new Experience(experience), instance, currentChapter);
     }
 
     @Override
